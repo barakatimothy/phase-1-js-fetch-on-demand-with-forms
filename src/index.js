dd2694a4cbc4c35c78d34361dbb47e910320a922
barakatimothy <timothybaraka39@gmail.com>
2023-06-26 09:29:38 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form').addEventListener('submit',(event)=>{
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID')
+    })
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then (response => response.json())
+    .then ( data =>{
+      const title = document.querySelector('section#movieDetails h4')
+      const summary= document.querySelector('section#movieDetails p')
 
-document.addEventListener('DOMContentLoaded', init);
+    title.textContent= data.title
+    summary.textContent = data.summary
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
